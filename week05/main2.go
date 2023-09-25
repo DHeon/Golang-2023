fd2cad49384c3468e915f2af2fac3f853d0aeea8
heon <jerrystar58@gmail.com>
2023-09-25 17:09:38 +0900

    v3

---
+++ week05/main2.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Print("Input Score: ")
+	reader := bufio.NewReader(os.Stdin)
+	inputScore := reader.ReadString('\n') // assignment mismatch: 1 variable but reader.ReadString returns 2 values
+	fmt.Println(inputScore)
+}
