bd5c774da9e3fc6a83047d9e24393e34a1130f20
heon <jerrystar58@gmail.com>
2023-09-25 17:33:12 +0900

    v7

--- week05/main2.go
+++ week05/main2.go
@@ -11,6 +11,13 @@ func main() {
 	fmt.Print("Input Score: ")
 	reader := bufio.NewReader(os.Stdin)
 	inputScore, err := reader.ReadString('\n') // option 2
-	log.Fatal(err)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(inputScore)
+	if inputScore >= 90 { //mismatched types string and untyped int
+		grade := "A grade!"
+	} else {
+		grade := "under A grade.."
+	}
 }
