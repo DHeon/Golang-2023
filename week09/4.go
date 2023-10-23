4cf24a85944c809ebe66a09098fe62cdb37c660b
heon <jerrystar58@gmail.com>
2023-10-23 17:50:55 +0900

    week 9 4.0

--- week09/4.go
+++ week09/4.go
@@ -7,8 +7,11 @@ func main() {
 	b := 20
 
 	c := &a
-	//fmt.Printf("%d %d %x\n", a, b, c) //x로 하면 16진수로 나오는듯.
-	fmt.Printf("%d %d %d\n", a, b, *c)
+
+	fmt.Printf("%T\n", c)
+	fmt.Println(a, b)
+	swap(&a, &b)
+	fmt.Println(a, b)
 
 }
 
