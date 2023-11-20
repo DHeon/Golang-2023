d03dd3a826d3ead1d33c78cd98d814aaafe66659
heon <jerrystar58@gmail.com>
2023-11-20 16:52:28 +0900

    1120-2

--- week13/main1.go
+++ week13/main1.go
@@ -4,6 +4,11 @@ import "fmt"
 
 func main() {
 	var a []string
-	fmt.Printf("%#v\n", a)
+	var b []bool //현재 공간이 없고 선언만 되있어서 nil상태임.
+	// a = make([]string, 4, 5)
+
+	b = append(b, true) //append하면 생김.
+	fmt.Printf("%#v %#v\n", a, b)
 	fmt.Println(a, len(a), cap(a))
+	fmt.Println(b, len(b), cap(b))
 }
